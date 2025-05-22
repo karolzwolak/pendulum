6c14555545371202125ce6519f4481b49cf06450
Karol Zwolak <karolzwolak7@gmail.com>
2025-05-22 22:01:17 +0200

    chore: use the model on cpu instead of gpu
    MlpPolicy supposedly runs better on cpu

--- model.py
+++ model.py
@@ -2,8 +2,8 @@ from stable_baselines3 import PPO
 
 
 def create_model(env):
-    return PPO("MlpPolicy", env, verbose=1)
+    return PPO("MlpPolicy", env, verbose=1, device="cpu")
 
 
 def load_model(model_path, env):
-    return PPO.load(model_path, env)
+    return PPO.load(model_path, env, device="cpu")
