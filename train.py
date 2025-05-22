c17ea6c22740ad3d7cfeb8d5c0ecc3f0f1bd02b0
Karol Zwolak <karolzwolak7@gmail.com>
2025-05-22 22:27:41 +0200

    feat: bump learning time and allow for early training exit

--- train.py
+++ train.py
@@ -3,13 +3,22 @@ from cartpole import CartPoleSimulation
 from env import Env
 from model import create_model
 
-if __name__ == "__main__":
+
+def main():
     sim = CartPoleSimulation()
     env = Env(sim)
 
     check_env(env, warn=True)
 
     model = create_model(env)
-    model.learn(total_timesteps=100_000)
-
+    try:
+        model.learn(total_timesteps=10_000_000)
+    except KeyboardInterrupt:
+        if input("Save model? (y/n): ").lower() == "y":
+            model.save("models/cartpole")
+        return
     model.save("models/cartpole")
+
+
+if __name__ == "__main__":
+    main()
