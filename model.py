9b78e9eb250664d598efa410e06641a1fc0cd78d
Karol Zwolak <karolzwolak7@gmail.com>
2025-05-24 18:20:18 +0200

    parallel envs

--- model.py
+++ model.py
@@ -1,4 +1,8 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from cartpole import CartPoleSimulation
+from env import Env
 
 
 def create_model(env):
@@ -7,3 +11,10 @@ def create_model(env):
 
 def load_model(model_path, env):
     return PPO.load(model_path, env, device="cpu")
+
+
+def make_parallel_envs(num_envs=4):
+    """Create multiple parallel environments for faster sampling"""
+    return make_vec_env(
+        lambda: Env(CartPoleSimulation()), n_envs=num_envs, vec_env_cls=SubprocVecEnv
+    )
--- train.py
+++ train.py
@@ -1,18 +1,19 @@
 from stable_baselines3.common.env_checker import check_env
 from cartpole import CartPoleSimulation
 from env import Env
-from model import create_model
+from model import create_model, make_parallel_envs
 
 
 def main():
-    sim = CartPoleSimulation()
-    env = Env(sim)
+    # Use parallel environments
+    env = make_parallel_envs(num_envs=4)
 
-    check_env(env, warn=True)
+    # Check one of the environments
+    check_env(Env(CartPoleSimulation()), warn=True)
 
     model = create_model(env)
     try:
-        model.learn(total_timesteps=10_000_000)
+        model.learn(total_timesteps=10_000_000, progress_bar=True)
     except KeyboardInterrupt:
         if input("Save model? (y/n): ").lower() == "y":
             model.save("models/cartpole")
