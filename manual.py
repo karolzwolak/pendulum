d44b6536201f2ae47261562b86080b17585f6759
Karol Zwolak <karolzwolak7@gmail.com>
2025-05-24 13:30:44 +0200

    add keybinds to the general renderer

--- manual.py
+++ manual.py
@@ -1,5 +1,3 @@
-import sys
-import pygame
 from renderer import Renderer
 from cartpole import CartPoleSimulation
 
@@ -8,30 +6,8 @@ class ManualRenderer(Renderer):
     def __init__(self, sim):
         super().__init__(sim)
 
-    def handle_input(self):
-        super().handle_input()
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            self.sim.apply_force(-1)
-        if keys[pygame.K_RIGHT]:
-            self.sim.apply_force(1)
-        if keys[pygame.K_r]:
-            self.sim.reset()
-        if keys[pygame.K_w]:
-            self.sim.reset(angle=3.14)
-        if keys[pygame.K_s]:
-            self.sim.reset(angle=0)
-        if keys[pygame.K_d]:
-            self.sim.reset(angle=3.14 * 1.5)
-        if keys[pygame.K_a]:
-            self.sim.reset(angle=3.14 * 0.5)
-        if keys[pygame.K_e]:
-            self.sim.reset(angle=-3.14 * 0.75)
-        if keys[pygame.K_q]:
-            self.sim.reset(angle=3.14 * 0.75)
-        if keys[pygame.K_t]:
-            self.sim.reset(self.sim.angle())
+    def update(self):
+        super().update()
         print("curr tick reward: ", self.sim.compute_reward())
 
 
--- renderer.py
+++ renderer.py
@@ -11,15 +11,18 @@ class Renderer:
         info = pygame.display.Info()
         pygame.display.set_caption("Pendulum Simulation")
         screen_size = (800, 600)
-        self.screen = pygame.display.set_mode((screen_size[0], screen_size[1]), pygame.RESIZABLE)
+        self.screen = pygame.display.set_mode(
+            (screen_size[0], screen_size[1]), pygame.RESIZABLE
+        )
 
         self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
-        scale_x = screen_size[0]/ (WORLD_SIZE * 2)
+        scale_x = screen_size[0] / (WORLD_SIZE * 2)
         scale_y = screen_size[1] / (WORLD_SIZE * 2)
         scale = min(scale_x, scale_y)
         offset_x = screen_size[0] / 2
         offset_y = screen_size[1] / 2
-        transform = pymunk.Transform(a=scale, d=scale, tx=offset_x, ty=offset_y)
+        transform = pymunk.Transform(
+            a=scale, d=scale, tx=offset_x, ty=offset_y)
         self.draw_options.transform = transform
 
         self.clock = pygame.time.Clock()
@@ -50,6 +53,28 @@ class Renderer:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.exit()
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.sim.apply_force(-1)
+        if keys[pygame.K_RIGHT]:
+            self.sim.apply_force(1)
+        if keys[pygame.K_r]:
+            self.sim.reset()
+        if keys[pygame.K_w]:
+            self.sim.reset(angle=3.14)
+        if keys[pygame.K_s]:
+            self.sim.reset(angle=0)
+        if keys[pygame.K_d]:
+            self.sim.reset(angle=3.14 * 1.5)
+        if keys[pygame.K_a]:
+            self.sim.reset(angle=3.14 * 0.5)
+        if keys[pygame.K_e]:
+            self.sim.reset(angle=-3.14 * 0.75)
+        if keys[pygame.K_q]:
+            self.sim.reset(angle=3.14 * 0.75)
+        if keys[pygame.K_t]:
+            self.sim.reset(self.sim.angle())
 
     def render(self):
         self.handle_input()
