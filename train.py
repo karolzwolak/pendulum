803429a5e569e4e51ac60de24a4aba5c45f58dc0
Karol Zwolak <karolzwolak7@gmail.com>
2025-05-29 10:18:14 +0200

    move currriculum callback to model.py

--- train.py
+++ train.py
@@ -1,76 +1,9 @@
-from stable_baselines3.common.callbacks import BaseCallback
-from collections import deque
-from tqdm import tqdm
-
-from model import create_model, make_parallel_normalized_envs, save_model
-
-
-class CurriculumLearningCallback(BaseCallback):
-    def __init__(
-        self,
-        stop_traning_reward_threshold: float = 70,
-        progress_curriculum_theshold: float = 70,
-        curriculum_steps: int = 1000,
-        n_episodes: int = 10,
-        verbose=0,
-    ):
-        super().__init__(verbose)
-        self.stop_traning_reward_threshold = stop_traning_reward_threshold
-        self.progress_curriculum_reward_threshold = progress_curriculum_theshold
-        self.n_episodes = n_episodes
-        self.episode_rewards = deque(maxlen=n_episodes)
-        self.curriculum_steps = curriculum_steps
-        self.curriculum_step = 0
-
-    def _on_training_start(self) -> None:
-        self.training_env.env_method("progress_curriculum", 0)
-        self.model._current_progress_remaining = 1.0
-        self.pbar = tqdm(
-            total=self.curriculum_steps,
-            desc="Curriculum Progress",
-            dynamic_ncols=True,
-        )
-
-    def _on_training_end(self) -> None:
-        self.pbar.close()
-
-    def _on_step(self) -> bool:
-        dones = self.locals.get("dones")
-        infos = self.locals.get("infos")
-
-        if dones is not None and infos is not None:
-            for done, info in zip(dones, infos):
-                if done and "episode" in info:
-                    ep_reward = info["episode"]["r"]
-                    self.episode_rewards.append(ep_reward)
-
-        if len(self.episode_rewards) < self.n_episodes:
-            return True
-
-        mean_reward = sum(self.episode_rewards) / self.n_episodes
-
-        finished_curriculum = self.curriculum_step >= self.curriculum_steps
-        if (
-            not finished_curriculum
-            and mean_reward >= self.progress_curriculum_reward_threshold
-        ):
-            self.curriculum_step += 1
-            t = self.curriculum_step / self.curriculum_steps
-
-            self.training_env.env_method("progress_curriculum", t)
-            self.episode_rewards.clear()
-
-            self._current_progress_remaining = 1.0 - t
-            self.pbar.n = self.curriculum_step
-            self.pbar.refresh()
-
-        if finished_curriculum and mean_reward >= self.stop_traning_reward_threshold:
-            print(
-                f"Threshold reached: mean reward = {mean_reward:.2f} ≥ {self.stop_traning_reward_threshold}"
-                ", stopping training."
-            )
-            return False
-        return True
+from model import (
+    create_model,
+    make_parallel_normalized_envs,
+    save_model,
+    CurriculumLearningCallback,
+)
 
 
 def main():
